3d11e6b4663922e3a7d646183e4527b8f9d268ab
Ian McLerran <imclerran@protonmail.com>
2024-06-10 15:47:11 -0500

    Add ci testing

---
+++ ci/all_tests.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
+
+roc='./roc_nightly/roc'
+
+package_dir='./package/'
+
+# roc check
+for roc_file in $package_dir*.roc; do
+    $roc check $roc_file
+done
+
+$roc test ./package/Test.roc
